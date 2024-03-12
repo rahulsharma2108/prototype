710321c2e5ee0bb77a7c70ca98cd17ec179eb936
rahulsharma2108 <rahulsharma2108@gmail.com>
2024-03-12 13:55:37 +0530

    Added base url

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,9 @@
+import { API_BASE_URL } from '../../common/utils'
 import './card.scss'
 
 export const Card = ({ img, title }) => {
     const imgPath = '/images/'
-    return <div className="card-container" style={{backgroundImage: `url(${imgPath}${img})`}}>
+    return <div className="card-container" style={{backgroundImage: `url(${API_BASE_URL}${imgPath}${img})`}}>
         <div className='card-content'>
             {title}
         </div>
